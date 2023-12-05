a791e3c2001c4db5ea05b828755f5d3ebde8fd19
Alexandr.Burmistroff <albook366314@gmail.com>
2023-12-05 16:53:32 +0100

    Создание фильтров

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,7 +1,23 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Flight> flights = FlightBuilder.createFlights();
+
+        FlightFilter departureBeforeNowFilter = new DepartureBeforeNowFilter();
+        List<Flight> filteredFlights1 = departureBeforeNowFilter.filterFlights(flights);
+        System.out.println("Включен фильтр по вылету до текущего момента времени: " + filteredFlights1);
+
+        FlightFilter arrivalBeforeDepartureFilter = new ArrivalBeforeDepartureFilter();
+        List<Flight> filteredFlights2 = arrivalBeforeDepartureFilter.filterFlights(flights);
+        System.out.println("Включен фильтр по дате прилета раньше даты вылета: " + filteredFlights2);
+
+        FlightFilter groundTimeMoreThanTwoHoursFilter = new GroundTimeMoreThanTwoHoursFilter();
+        List<Flight> filteredFlights3 = groundTimeMoreThanTwoHoursFilter.filterFlights(flights);
+        System.out.println("Включен фильтр по общему времени на земле более двух часов: " + filteredFlights3);
+
+        System.out.println("Исходные тестовые данные без фильтров:" + flights);
     }
 }
