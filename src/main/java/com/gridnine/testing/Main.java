2e896b4ba2351a103414040c0967818a20e3e8a3
Alexandr.Burmistroff <albook366314@gmail.com>
2023-12-06 16:40:52 +0100

    Исправление ошибок, написание тестов

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -7,16 +7,19 @@ public class Main {
         List<Flight> flights = FlightBuilder.createFlights();
 
         FlightFilter departureBeforeNowFilter = new DepartureBeforeNowFilter();
-        List<Flight> filteredFlights1 = departureBeforeNowFilter.filterFlights(flights);
-        System.out.println("Включен фильтр по вылету до текущего момента времени: " + filteredFlights1);
+        List<Flight> filteredFlightsByDepartureBeforeNow= departureBeforeNowFilter.filterFlights(flights);
+        System.out.println("Включен фильтр по вылету до текущего момента времени: "
+                + filteredFlightsByDepartureBeforeNow);
 
         FlightFilter arrivalBeforeDepartureFilter = new ArrivalBeforeDepartureFilter();
-        List<Flight> filteredFlights2 = arrivalBeforeDepartureFilter.filterFlights(flights);
-        System.out.println("Включен фильтр по дате прилета раньше даты вылета: " + filteredFlights2);
+        List<Flight> filteredFlightsByArrivalBeforeDeparture = arrivalBeforeDepartureFilter.filterFlights(flights);
+        System.out.println("Включен фильтр по дате прилета раньше даты вылета: "
+                + filteredFlightsByArrivalBeforeDeparture);
 
         FlightFilter groundTimeMoreThanTwoHoursFilter = new GroundTimeMoreThanTwoHoursFilter();
-        List<Flight> filteredFlights3 = groundTimeMoreThanTwoHoursFilter.filterFlights(flights);
-        System.out.println("Включен фильтр по общему времени на земле более двух часов: " + filteredFlights3);
+        List<Flight> filteredFlightsByGroundTimeMoreThanTwoHours = groundTimeMoreThanTwoHoursFilter.filterFlights(flights);
+        System.out.println("Включен фильтр по общему времени на земле более двух часов: "
+                + filteredFlightsByGroundTimeMoreThanTwoHours);
 
         System.out.println("Исходные тестовые данные без фильтров:" + flights);
     }
